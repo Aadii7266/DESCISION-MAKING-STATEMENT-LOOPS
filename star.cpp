ad4be73508fedbd3047609a60453063746a65962
Aadii7266 <adityaparihar7266@gmail.com>
2024-10-11 20:56:29 +0530

    Add files via upload

---
+++ star.cpp
@@ -0,0 +1,28 @@
+//AADITYA PARIHAR
+//23070123001
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter your choice: ";
+    cin>>n;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+/* OUTPUT:
+Enter your choice: 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
