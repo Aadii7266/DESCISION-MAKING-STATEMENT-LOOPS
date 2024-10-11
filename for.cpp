2424c790e56a6a9f6fd52c5002271f2e420284bf
Aadii7266 <adityaparihar7266@gmail.com>
2024-10-11 21:00:00 +0530

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,19 @@
+//AADITYA PARIHAR
+//23070123001
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        cout<<"SIT"<<endl;
+    }
+}
+
+/* OUTPUT:
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
